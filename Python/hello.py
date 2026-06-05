c4c6a4360fc280ece26c59e2c13c2958de5232f1
Gayathri7841 <boinagayathri1201@gmail.com>
2026-06-05 15:42:48 +0530

    Create hello.py

---
+++ Python/hello.py
@@ -0,0 +1,25 @@
+#first print statement
+print("hello world!")
+#using semicolns doest show error but rarely used
+print("MY NAME IS GAYATHRI");
+#can use semicolons to write multiple statements in one line 
+print("hello world"); print("hello");
+
+#text can be printed by using single quotes
+print('Text in single quotes')
+
+#by default text inside print gets printed in new line so we use end to make 2 print statements in one line
+print("Hello this is print statement 1", end=" ")
+print("and this is in print statement 2")
+
+# print statement is also used to print numbers
+print(3)
+print(4)
+print(5)
+
+#mix of text and numbers
+print("Iam Gayathri ", 21 ,"years old")
+
+""" this is a comment
+multiline 
+comment  """
