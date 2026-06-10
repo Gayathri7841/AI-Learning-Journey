463792ff3b690381b8a697d223e98d0f8deebb8b
Gayathri7841 <boinagayathri1201@gmail.com>
2026-06-10 21:42:03 +0530

    Update lists.py

--- Python/lists.py
+++ Python/lists.py
@@ -47,3 +47,37 @@ print(list)
 l.clear()
 print(l)
 
+list=[10,20,30,40,50,60]
+for i in range(2,5):
+    print(list[i])
+
+[print(x) for x in list]#list comprehension
+
+
+#adding elements into list
+print(list)
+list.append(70)
+print(list)
+list.insert(2,25)
+print(list)
+list2=[80,90,100,110,120]
+
+list.extend(list2)
+print(list)
+
+list[1:3]=[5,15]#gave sufficient values
+print(list)
+list[1:3]=[25,35,45]#gave more values
+print(list)
+list[1:4]=[0,8]#gave less values
+print(list)
+
+list.remove(120)#removes given ele 
+print(list)
+
+list.pop(2)#removes ele at given index
+print(list)
+list.sort()
+print(list)
+
+
